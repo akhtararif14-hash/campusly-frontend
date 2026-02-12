61af8b9a635f8feca9ef12d77dc90899778c989f
akhtararif14 <akhtararif14@gmail.com>
2026-02-12 10:04:58 +0530

    hrl

--- src/pages/DashboardHome.jsx
+++ src/pages/DashboardHome.jsx
@@ -1,17 +1,26 @@
 const DashboardHome = () => {
   return (
-    <div className="space-y-10">
-      <div className="p-4 rounded-3xl">
-        Welcome to Campusly Lorem ipsum dolor sit amet consectetur adipisicing elit.
+    <div className="space-y-3">
+      <div className="border-1 p-4 rounded-4xl bg-blue-500 text-white px-4 py-3 flex items-center gap-4">
+       <button className="bg-white text-black px-4 py-2 rounded-4xl hover:bg-gray-200 transition-colors">
+        Expolre
+       </button>
+       <button className="bg-white text-black px-4 py-2 rounded-4xl hover:bg-gray-200 transition-colors">
+        Events Near You
+       </button>
+        <input type="button" value="Search" className="bg-white text-black px-60 py-2 rounded-4xl hover:bg-gray-200 transition-colors" />
+        <button className="bg-white text-black px-4 py-2 rounded-4xl hover:bg-gray-200 transition-colors">
+        Messages
+       </button>
+       <button className="bg-white text-black px-4 py-2 rounded-4xl hover:bg-gray-200 transition-colors">
+        Connections
+       </button>
+       <button className="bg-white text-black px-4 py-2 rounded-4xl hover:bg-gray-200 transition-colors">
+        Trending Topics In JMI
+       </button>
       </div>
-
-      <div className="border-2 p-4 rounded-3xl bg-blue-500 text-white">
-        Dashboard stats / announcements
-      </div>
-
-      <div className="border-2 p-4 rounded-3xl bg-blue-500 text-white">
-        Upcoming events
-      </div>
+      <div className="border-1 border-black p-4 rounded-4xl text-white h-[80vh] flex items-center gap-4">
+        </div>
     </div>
   )
 }
