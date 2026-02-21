7a6d42a217a10b1123b4dda1536a066b2f408d2b
akhtararif14 <akhtararif14@gmail.com>
2026-02-21 16:20:17 +0530

    hrllo

--- src/pages/BuySell.jsx
+++ src/pages/BuySell.jsx
@@ -36,7 +36,7 @@ export default function BuySell() {
 
         <div className="relative">
           <img
-            src="/images/cart2.svg"
+            src="/images/cart-svg.svg"
             alt="cart"
             className="w-15 h-15 p-2 rounded-3xl cursor-pointer"
             onClick={() => navigate("/cart")}
